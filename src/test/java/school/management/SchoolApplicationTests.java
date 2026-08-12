243994942f6a02240b7a2bf30b3f8b48db1e54e8
venkateshsakiri <86957958+venkateshsakiri@users.noreply.github.com>
2026-08-12 15:57:32 +0530

    Add comment for automatic build commit testing
    Added a comment for testing automatic build commit.

--- src/test/java/school/management/SchoolApplicationTests.java
+++ src/test/java/school/management/SchoolApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SchoolApplicationTests {
-
+    // testing for auto matic build commit
 	@Test
 	void contextLoads() {
 	}
